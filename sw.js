dca38691605c8d6b4b5bdbe5b878f40e151cd4b3
SUASTRA50 <65449969+SUASTRA50@users.noreply.github.com>
2026-09-10 16:02:01 +0700

    Refactor service worker event listeners and caching

--- sw.js
+++ sw.js
@@ -2,32 +2,29 @@ const CACHE_NAME = 'laundry-pos-v16';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))
-  );
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    }).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((cached) => {
-      return cached || fetch(e.request).then((res) => {
-        // cache copy
-        return res;
-      }).catch(() => cached);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
